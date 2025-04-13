c7c33aa90ee450a7d82bc5a2baa6edf1eeece81d
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2025-04-12 22:48:24 -0400

    Uncomment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
     if (args.length != 2 || !args[0].equals("-E")) {
       System.out.println("Usage: ./your_program.sh -E <pattern>");
       System.exit(1);
@@ -12,16 +16,11 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     String inputLine = scanner.nextLine();
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    // 
-    // if (matchPattern(inputLine, pattern)) {
-    //     System.exit(0);
-    // } else {
-    //     System.exit(1);
-    // }
+    if (matchPattern(inputLine, pattern)) {
+         System.exit(0);
+    } else {
+         System.exit(1);
+    }
   }
 
   public static boolean matchPattern(String inputLine, String pattern) {
